435628d7714d8a00166a24ca48a5caf65e2bee73
James Welle <jwelle@articulate.com>
2016-06-14 16:35:16 -0600

    appveyor (#3)
    - Added appveyor build configuration

--- System.Doubles/Properties/AssemblyInfo.cs
+++ System.Doubles/Properties/AssemblyInfo.cs
@@ -1,9 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyTitle("System.Doubles")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyFileVersion("6.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
