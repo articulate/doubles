08580f8f41d2b1ea88a83145c78917e75abeb237
rwhite <rjacobw@gmail.com>
2016-08-18 10:13:45 -0700

    System.Windows.Forms: Add IScreen and ScreenWrapper

---
+++ System.Doubles/Windows/Forms/ScreenWrapper.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Windows.Forms
+{
+    public sealed class ScreenWrapper : IScreen
+    {
+        public Screen FromPoint(Drawing.Point point)
+        {
+            return Screen.FromPoint(point);
+        }
+    }
+}
