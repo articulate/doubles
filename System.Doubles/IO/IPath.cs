57f313bd17ed46724e0ff5237cc631ed97afcaff
James Welle <jwelle@articulate.com>
2016-08-23 09:08:58 -0600

    IPath.Combine
     - Added IPath.Combine

--- System.Doubles/IO/IPath.cs
+++ System.Doubles/IO/IPath.cs
@@ -7,5 +7,13 @@
         string GetFileNameWithoutExtension(string path);
 
         string GetExtension(string path);
+
+        string Combine(string path1, string path2);
+
+        string Combine(string path1, string path2, string path3);
+
+        string Combine(string path1, string path2, string path3, string path4);
+
+        string Combine(params string[] paths);
     }
 }
--- System.Doubles/IO/PathWrapper.cs
+++ System.Doubles/IO/PathWrapper.cs
@@ -16,5 +16,25 @@ namespace System.IO
         {
             return Path.GetExtension(path);
         }
+
+        public string Combine(string path1, string path2)
+        {
+            return Path.Combine(path1, path2);
+        }
+
+        public string Combine(string path1, string path2, string path3)
+        {
+            return Path.Combine(path1, path2, path3);
+        }
+
+        public string Combine(string path1, string path2, string path3, string path4)
+        {
+            return Path.Combine(path1, path2, path3, path4);
+        }
+
+        public string Combine(params string[] paths)
+        {
+            return Path.Combine(paths);
+        }
     }
 }
