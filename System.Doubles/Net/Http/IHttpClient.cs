e7e80b041ed2f4fa417421f065db614ad038c230
James Welle <jwelle@articulate.com>
2016-09-21 13:16:37 -0600

    HttpClient
     - Added System.Net.Http.IHttpClient and the wrapper implementation

---
+++ System.Doubles/Net/Http/IHttpClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Net.Http
+{
+    public interface IHttpClient : IDisposable
+    {
+        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, HttpCompletionOption httpCompletionOption, CancellationToken cancellationToken);
+    }
+}
