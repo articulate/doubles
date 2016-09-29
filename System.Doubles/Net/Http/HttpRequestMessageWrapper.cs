919b8d8080d5a6abd2918d86deb8cae9d92a0782
James Welle <jwelle@articulate.com>
2016-09-29 09:24:35 -0600

    IHttpRequestMessage and IHttpResponseMessage (#11)
    - Added IHttpRequestMessage and IHttpResponseMessage in System.Net.Http

---
+++ System.Doubles/Net/Http/HttpRequestMessageWrapper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace System.Net.Http
+{
+    public sealed class HttpRequestMessageWrapper : IHttpRequestMessage
+    {
+        public Version Version => httpRequestMessage.Version;
+
+        public IHttpContent Content
+        {
+            get;
+        }
+
+        public HttpMethod Method => httpRequestMessage.Method;
+
+        public Uri RequestUri => httpRequestMessage.RequestUri;
+
+        public IDictionary<string, object> Properties => httpRequestMessage.Properties;
+
+        private readonly HttpRequestMessage httpRequestMessage;
+
+        public HttpRequestMessageWrapper(HttpRequestMessage httpRequestMessage)
+        {
+            this.httpRequestMessage = httpRequestMessage;
+
+            Content = httpRequestMessage.Content == null ? null : new HttpContentWrapper(httpRequestMessage.Content);
+        }
+
+        public void Dispose() => httpRequestMessage.Dispose();
+
+        public override string ToString() => httpRequestMessage.ToString();
+    }
+}
