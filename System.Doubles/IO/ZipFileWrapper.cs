df3511139d0c8c6687d3acacf1267e92e3cbe1ec
Matt Winckler <mwinckler@users.noreply.github.com>
2017-02-08 12:49:45 -0800

    Add ZipFileWrapper (#17)

---
+++ System.Doubles/IO/ZipFileWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System.IO.Compression;
+using System.Text;
+
+namespace System.IO
+{
+    public class ZipFileWrapper : IZipFile
+    {
+        public void CreateFromDirectory(string sourceDirectoryName, string destinationArchiveFileName, CompressionLevel compressionLevel, bool includeBaseDirectory, Encoding entryNameEncoding)
+        {
+            ZipFile.CreateFromDirectory(sourceDirectoryName, destinationArchiveFileName, compressionLevel, includeBaseDirectory, entryNameEncoding);
+        }
+    }
+}
