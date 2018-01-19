60b6546891360d5cdc1b2713bb1a64659bde441a
James Welle <jwelle@articulate.com>
2018-01-19 15:00:42 -0700

    System.IO.IStreamReader (#21)

---
+++ System.Doubles/IO/StreamReaderWrapper.cs
@@ -0,0 +1,17 @@
+﻿namespace System.IO
+{
+    public sealed class StreamReaderWrapper : IStreamReader
+    {
+        public bool EndOfStream => streamReader.EndOfStream;
+
+        private readonly StreamReader streamReader;
+
+        public StreamReaderWrapper(StreamReader streamReader) => this.streamReader = streamReader;
+
+        public void Dispose() => streamReader.Dispose();
+
+        public string ReadLine() => streamReader.ReadLine();
+
+        public string ReadToEnd() => streamReader.ReadToEnd();
+    }
+}
