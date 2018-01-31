18b22c2f3d0d5173fd4cb186d04c8a5d3ce75f9b
Jerry Lin <jclin@articulate.com>
2018-01-31 11:55:28 -0800

    Add required properties and methods to IServiceController

--- System.Doubles/ServiceProcess/IServiceController.cs
+++ System.Doubles/ServiceProcess/IServiceController.cs
@@ -2,8 +2,25 @@
 {
     public interface IServiceController : IDisposable
     {
+        string ServiceName
+        {
+            get;
+        }
+
+        ServiceControllerStatus Status
+        {
+            get;
+        }
+
+        string DisplayName
+        {
+            get;
+        }
+
         void Start();
 
         void Stop();
+
+        void Refresh();
     }
 }
