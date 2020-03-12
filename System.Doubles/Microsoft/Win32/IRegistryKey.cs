bd8b1498fe4c4392a4dbdeddeb789960833b4792
Matt Winckler <mwinckler@articulate.com>
2020-03-11 17:04:27 -0700

    Expose additional methods on IRegistryKey

--- System.Doubles/Microsoft/Win32/IRegistryKey.cs
+++ System.Doubles/Microsoft/Win32/IRegistryKey.cs
@@ -9,10 +9,16 @@ namespace Microsoft.Win32
 
         IRegistryKey OpenSubKey(string keyName);
 
+        IRegistryKey CreateSubKey(string keyName);
+
         RegistryValueKind GetValueKind(string valueName);
 
         object GetValue(string valueName, object defaultValue);
 
         void SetValue(string valueName, object value, RegistryValueKind registryValueKind);
+
+        void DeleteValue(string valueName, bool throwOnMissingValue);
+
+        void Close();
     }
 }
